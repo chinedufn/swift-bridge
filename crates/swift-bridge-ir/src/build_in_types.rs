dcab11f56c92af4168d0c491b58a5b923fcf9f38
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2021-11-17 11:32:22 -0500

    Add built in support for pointers.

--- crates/swift-bridge-ir/src/build_in_types.rs
+++ crates/swift-bridge-ir/src/build_in_types.rs
@@ -1,7 +1,7 @@
 use quote::ToTokens;
 use syn::Type;
 
-#[derive(Debug, PartialEq, Copy, Clone)]
+#[derive(Debug, PartialEq, Clone)]
 pub(crate) enum BuiltInType {
     U8,
     I8,
@@ -17,6 +17,19 @@ pub(crate) enum BuiltInType {
     Isize,
     F32,
     F64,
+    Pointer(BuiltInTypePointer),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) struct BuiltInTypePointer {
+    pub kind: PointerKind,
+    pub ty: Box<BuiltInType>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) enum PointerKind {
+    Const,
+    Mut,
 }
 
 impl BuiltInType {
@@ -24,6 +37,20 @@ impl BuiltInType {
         match ty {
             Type::Path(path) => {
                 Self::with_str(path.path.segments.to_token_stream().to_string().as_str())
+            }
+            Type::Ptr(ptr) => {
+                let kind = if ptr.const_token.is_some() {
+                    PointerKind::Const
+                } else {
+                    PointerKind::Mut
+                };
+
+                Self::with_type(&ptr.elem).map(|ty| {
+                    Self::Pointer(BuiltInTypePointer {
+                        kind,
+                        ty: Box::new(ty),
+                    })
+                })
             }
             _ => None,
         }
@@ -50,42 +77,48 @@ impl BuiltInType {
         return Some(ty);
     }
 
-    pub fn to_swift(&self) -> &'static str {
+    pub fn to_swift(&self) -> String {
         match self {
-            BuiltInType::U8 => "UInt8",
-            BuiltInType::I8 => "Int8",
-            BuiltInType::U16 => "UInt16",
-            BuiltInType::I16 => "Int16",
-            BuiltInType::U32 => "UInt32",
-            BuiltInType::I32 => "Int32",
-            BuiltInType::U64 => "UInt64",
-            BuiltInType::I64 => "Int64",
-            BuiltInType::U128 => "UInt128",
-            BuiltInType::I128 => "Int128",
-            BuiltInType::F32 => "Float",
-            BuiltInType::F64 => "Double",
-            BuiltInType::Usize => "UInt",
-            BuiltInType::Isize => "Int",
+            BuiltInType::U8 => "UInt8".to_string(),
+            BuiltInType::I8 => "Int8".to_string(),
+            BuiltInType::U16 => "UInt16".to_string(),
+            BuiltInType::I16 => "Int16".to_string(),
+            BuiltInType::U32 => "UInt32".to_string(),
+            BuiltInType::I32 => "Int32".to_string(),
+            BuiltInType::U64 => "UInt64".to_string(),
+            BuiltInType::I64 => "Int64".to_string(),
+            BuiltInType::U128 => "UInt128".to_string(),
+            BuiltInType::I128 => "Int128".to_string(),
+            BuiltInType::F32 => "Float".to_string(),
+            BuiltInType::F64 => "Double".to_string(),
+            BuiltInType::Usize => "UInt".to_string(),
+            BuiltInType::Isize => "Int".to_string(),
+            BuiltInType::Pointer(ptr) => {
+                format!("UnsafeMutablePointer<{}>", ptr.ty.to_swift())
+            }
         }
     }
 
+    // FIXME: Delete this
     pub fn to_c(&self) -> &'static str {
-        match self {
-            BuiltInType::U8 => "uint8_t",
-            BuiltInType::I8 => "int8_t",
-            BuiltInType::U16 => "uint16_t",
-            BuiltInType::I16 => "int16_t",
-            BuiltInType::U32 => "uint32_t",
-            BuiltInType::I32 => "int32_t",
-            BuiltInType::U64 => "uint64_t",
-            BuiltInType::I64 => "int64_t",
-            BuiltInType::U128 => "uint128_t",
-            BuiltInType::I128 => "i128_t",
-            BuiltInType::F32 => "float",
-            BuiltInType::F64 => "double",
-            BuiltInType::Usize => "uintptr_t",
-            BuiltInType::Isize => "intptr_t",
-        }
+        // match self {
+        //     BuiltInType::U8 => "uint8_t",
+        //     BuiltInType::I8 => "int8_t",
+        //     BuiltInType::U16 => "uint16_t",
+        //     BuiltInType::I16 => "int16_t",
+        //     BuiltInType::U32 => "uint32_t",
+        //     BuiltInType::I32 => "int32_t",
+        //     BuiltInType::U64 => "uint64_t",
+        //     BuiltInType::I64 => "int64_t",
+        //     BuiltInType::U128 => "uint128_t",
+        //     BuiltInType::I128 => "i128_t",
+        //     BuiltInType::F32 => "float",
+        //     BuiltInType::F64 => "double",
+        //     BuiltInType::Usize => "uintptr_t",
+        //     BuiltInType::Isize => "intptr_t",
+        //     _ => todo!()
+        // }
+        unimplemented!("Delete this")
     }
 
     pub fn is_int(&self) -> bool {
@@ -131,6 +164,20 @@ mod tests {
             (quote! {isize}, BuiltInType::Isize),
             (quote! {f32}, BuiltInType::F32),
             (quote! {f64}, BuiltInType::F64),
+            (
+                quote! {*const u8},
+                BuiltInType::Pointer(BuiltInTypePointer {
+                    kind: PointerKind::Const,
+                    ty: Box::new(BuiltInType::U8),
+                }),
+            ),
+            (
+                quote! {*mut f64},
+                BuiltInType::Pointer(BuiltInTypePointer {
+                    kind: PointerKind::Mut,
+                    ty: Box::new(BuiltInType::F64),
+                }),
+            ),
         ];
         for (tokens, expected) in tests {
             let ty: Type = parse_quote! {#tokens};
