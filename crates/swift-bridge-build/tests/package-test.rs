bd0cb230fb23688cacafa10c53d35fb749506016
Jomy10 <jonas.vbs4@gmail.com>
2022-03-09 12:28:21 +0100

    added create generated folder

--- crates/swift-bridge-build/tests/package-test.rs
+++ crates/swift-bridge-build/tests/package-test.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::fs;
+use std::io::ErrorKind;
 use std::path::Path;
 use std::process::Command;
 
@@ -8,11 +10,19 @@ use swift_bridge_build::ApplePlatform as Platform;
 #[test]
 fn gen_package() {
     // Build Rust
-    Command::new("sh")
+    if let Err(err) = fs::create_dir("tests/sample_project/generated") {
+        if err.kind() != ErrorKind::AlreadyExists {
+            panic!("{}", err);
+        }
+    }
+    
+    let output = Command::new("sh")
         .current_dir("tests/sample_project")
         .arg("build.sh")
-        .spawn()
+        .output()
         .expect("Couldn't execute build script");
+    println!("{}", std::str::from_utf8(&*output.stdout).unwrap());
+    
     // Generate package
     generate_package(GeneratePackageConfig {
         bridge_dir: &Path::new("tests/sample_project/generated"),
