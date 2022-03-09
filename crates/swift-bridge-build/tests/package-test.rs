d398de8cd07ea89d9e5e19fee66818e627e1e0e5
Jomy10 <jonas.vbs4@gmail.com>
2022-03-09 13:44:50 +0100

    rename test projects

--- crates/swift-bridge-build/tests/package-test.rs
+++ crates/swift-bridge-build/tests/package-test.rs
@@ -10,14 +10,14 @@ use swift_bridge_build::ApplePlatform as Platform;
 #[test]
 fn gen_package() {
     // Build Rust
-    if let Err(err) = fs::create_dir("tests/sample_project/generated") {
+    if let Err(err) = fs::create_dir("tests/sample-project/generated") {
         if err.kind() != ErrorKind::AlreadyExists {
             panic!("{}", err);
         }
     }
     
     let output = Command::new("sh")
-        .current_dir("tests/sample_project")
+        .current_dir("tests/sample-project")
         .arg("build.sh")
         .output()
         .expect("Couldn't execute build script");
@@ -25,19 +25,19 @@ fn gen_package() {
     
     // Generate package
     generate_package(GeneratePackageConfig {
-        bridge_dir: &Path::new("tests/sample_project/generated"),
+        bridge_dir: &Path::new("tests/sample-project/generated"),
         paths: HashMap::from([
-            (Platform::iOS, &Path::new("tests/sample_project/target/x86_64-apple-ios/debug/libsample_project.a") as &dyn AsRef<Path>),
-            (Platform::macOS, &Path::new("tests/sample_project/target/x86_64-apple-darwin/debug/libsample_project.a") as &dyn AsRef<Path>),
-            (Platform::Simulator, &Path::new("tests/sample_project/target/aarch64-apple-ios/debug/libsample_project.a") as &dyn AsRef<Path>),
+            (Platform::iOS, &Path::new("tests/sample-project/target/x86_64-apple-ios/debug/libsample_project.a") as &dyn AsRef<Path>),
+            (Platform::macOS, &Path::new("tests/sample-project/target/x86_64-apple-darwin/debug/libsample_project.a") as &dyn AsRef<Path>),
+            (Platform::Simulator, &Path::new("tests/sample-project/target/aarch64-apple-ios/debug/libsample_project.a") as &dyn AsRef<Path>),
         ]),
-        out_dir: &Path::new("tests/sample_project/MySwiftPackage"),
+        out_dir: &Path::new("tests/sample-project/MySwiftPackage"),
         package_name: "MySwiftPackage"
     });
     
     // Test package (macOS executable)
     let output = Command::new("swift")
-        .current_dir("tests/test_project")
+        .current_dir("tests/test-project")
         .arg("run")
         .output()
         .expect("Failed to execute `swift run`");
