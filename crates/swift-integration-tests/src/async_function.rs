5f34ebfb26522b5bc802269feb8e52c5b563d751
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2022-04-19 21:48:03 -0400

    Support async methods (&self) (#52)
    For example, the following is now possible:
    
    ```rust
    #[swift_bridge::bridge]
    mod ffi {
        extern "Rust" {
            type SomeType;
            async fn some_async_method (&self);
        }
    }
    ```

--- crates/swift-integration-tests/src/async_function.rs
+++ crates/swift-integration-tests/src/async_function.rs
@@ -6,6 +6,15 @@ mod ffi {
         async fn rust_async_return_null();
         async fn rust_async_reflect_u8(arg: u8) -> u8;
         async fn rust_async_return_struct() -> AsyncRustFnReturnStruct;
+
+    }
+
+    extern "Rust" {
+        type TestRustAsyncSelf;
+
+        #[swift_bridg(init)]
+        fn new() -> TestRustAsyncSelf;
+        async fn reflect_u16(&self, arg: u16) -> u16;
     }
 }
 
@@ -17,4 +26,16 @@ async fn rust_async_reflect_u8(arg: u8) -> u8 {
 
 async fn rust_async_return_struct() -> ffi::AsyncRustFnReturnStruct {
     ffi::AsyncRustFnReturnStruct
+}
+
+pub struct TestRustAsyncSelf;
+
+impl TestRustAsyncSelf {
+    fn new() -> Self {
+        TestRustAsyncSelf
+    }
+
+    async fn reflect_u16(&self, arg: u16) -> u16 {
+        arg
+    }
 }
