cbcafaf26878253314b6273f2b4d58af55ed1adc
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2022-05-10 18:20:33 -0400

    Support return_into attribute on enums

--- crates/swift-integration-tests/src/function_attributes/return_into.rs
+++ crates/swift-integration-tests/src/function_attributes/return_into.rs
@@ -7,6 +7,10 @@ mod ffi {
     #[swift_bridge(already_declared, swift_repr = "struct")]
     struct AlreadyDeclaredStruct;
 
+    enum SomeTransparentEnum {
+        Variant,
+    }
+
     extern "Rust" {
         type SomeType;
 
@@ -24,6 +28,11 @@ mod ffi {
         // shared struct.
         #[swift_bridge(return_into)]
         fn get_already_declared_struct() -> AlreadyDeclaredStruct;
+
+        // Verify that our code compiles when we use `return_into` on an already declared
+        // shared struct.
+        #[swift_bridge(return_into)]
+        fn get_transparent_enum() -> SomeTransparentEnum;
     }
 }
 #[swift_bridge::bridge]
@@ -45,6 +54,15 @@ fn get_struct() -> SomeType {
 
 fn get_already_declared_struct() -> SomeType {
     SomeType
+}
+
+fn get_transparent_enum() -> u32 {
+    123
+}
+impl Into<ffi::SomeTransparentEnum> for u32 {
+    fn into(self) -> ffi::SomeTransparentEnum {
+        ffi::SomeTransparentEnum::Variant
+    }
 }
 
 impl Into<SomeType> for AnotherType {
