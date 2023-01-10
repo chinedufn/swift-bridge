fb6e46f5d6cb89fb5f24c4a4a8eff008c63456a0
niwaka <te1823ya@gmail.com>
2023-01-10 22:49:08 +0900

    Split test codes

---
+++ test-swift-packages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Swift package tests
+
+set -e
+
+export RUSTFLAGS="-D warnings"
+
+# Change to the root directory of the Xcode project
+cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P
+THIS_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+ROOT_DIR="$THIS_DIR"
+cd $ROOT_DIR
+
+# Make a temp directory
+TEMP_DIR=$(mktemp -d)
+
+# Delete the temp directory before the shell exits
+trap 'rm -rf $TEMP_DIR' EXIT
+
+# Copy directories related to all of the building and test running to the temp directory
+for DIR in crates src examples SwiftRustIntegrationTestRunner
+do
+  cp -r $DIR/ $TEMP_DIR/$DIR 
+done
+cp Cargo.toml $TEMP_DIR
+cd $TEMP_DIR/SwiftRustIntegrationTestRunner
+
+# Build Rust
+mkdir swift-package-rust-library-fixture/generated || true
+
+./swift-package-rust-library-fixture/build.sh
+
+# Create Swift Package
+cargo run -p integration-test-create-swift-package
+
+# Test Swift Package
+cd swift-package-test-package
+swift test
--- test-swift-rust-integration.sh
+++ test-swift-rust-integration.sh
@@ -12,26 +12,6 @@ ROOT_DIR="$THIS_DIR"
 cd $ROOT_DIR
 
 cd SwiftRustIntegrationTestRunner
-
-# Delete previous generated files/folders
-rm -r swift-package-rust-library-fixture/generated || true
-rm -r swift-package-rust-library-fixture/MySwiftPackage || true
-rm -r swift-package-rust-library-fixture/target || true
-rm -r swift-package-test-package/.build || true
-
-# Build Rust
-mkdir swift-package-rust-library-fixture/generated
-
-./swift-package-rust-library-fixture/build.sh
-
-# Create Swift Package
-cargo run -p integration-test-create-swift-package
-
-# Test Swift Package
-cd swift-package-test-package
-swift test
-cd ..
-
 # If project files don't exist before Xcode begins building we get something like:
 # error: Build input file cannot be found: '/path/to/Generated/SwiftBridgeCore.swift'
 # So.. here we create empty versions of the files that will get generated during the
