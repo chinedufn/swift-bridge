19dca2d93c1e1f6e171892ba5a970c3506620d42
niwaka <te1823ya@gmail.com>
2023-02-21 08:23:28 +0900

    add unit test

--- crates/swift-integration-tests/src/shared_types/shared_enum.rs
+++ crates/swift-integration-tests/src/shared_types/shared_enum.rs
@@ -18,6 +18,21 @@ mod ffi {
     extern "Rust" {
         fn reflect_enum_with_unnamed_data(arg: EnumWithUnnamedData) -> EnumWithUnnamedData;
     }
+
+    enum EnumWithNamedData {
+        Variant1{
+            hello: String, 
+            data_u8: u8,
+        },
+        Variant2{
+            data_i32: i32,
+        },
+        Variant3,
+    }
+
+    extern "Rust" {
+        fn reflect_enum_with_named_data(arg: EnumWithNamedData) -> EnumWithNamedData;
+    }
 }
 
 fn reflect_enum_with_no_data(arg: ffi::EnumWithNoData) -> ffi::EnumWithNoData {
@@ -25,5 +40,9 @@ fn reflect_enum_with_no_data(arg: ffi::EnumWithNoData) -> ffi::EnumWithNoData {
 }
 
 fn reflect_enum_with_unnamed_data(arg: ffi::EnumWithUnnamedData) -> ffi::EnumWithUnnamedData {
+    arg
+}
+
+fn reflect_enum_with_named_data(arg: ffi::EnumWithNamedData) -> ffi::EnumWithNamedData {
     arg
 }
