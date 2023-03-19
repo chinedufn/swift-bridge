fdca9fcc2080a60d30de958fc4ada2516691581e
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2023-03-18 21:52:02 -0400

    Support Option<Primitive> in Swift fns (#203)
    This commit adds support for Option<PrimitiveType> in `extern "Swift"`
    functions.
    
    For example, the following is now possible:
    
    ```rust
    mod ffi {
        extern "Swift" {
            fn my_func(arg: Option<u8>) -> Option<bool>;
        }
    }
    ```

--- SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunner/Option.swift
+++ SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunner/Option.swift
@@ -7,10 +7,43 @@
 
 import Foundation
 
-func create_swift_option_u8_some() -> Optional<UInt8> {
-    55
+func swift_reflect_option_u8(arg: Optional<UInt8>) -> Optional<UInt8> {
+    arg
+}
+func swift_reflect_option_i8(arg: Optional<Int8>) -> Optional<Int8> {
+    arg
+}
+func swift_reflect_option_u16(arg: Optional<UInt16>) -> Optional<UInt16> {
+    arg
+}
+func swift_reflect_option_i16(arg: Optional<Int16>) -> Optional<Int16> {
+    arg
+}
+func swift_reflect_option_u32(arg: Optional<UInt32>) -> Optional<UInt32> {
+    arg
+}
+func swift_reflect_option_i32(arg: Optional<Int32>) -> Optional<Int32> {
+    arg
+}
+func swift_reflect_option_u64(arg: Optional<UInt64>) -> Optional<UInt64> {
+    arg
+}
+func swift_reflect_option_i64(arg: Optional<Int64>) -> Optional<Int64> {
+    arg
+}
+func swift_reflect_option_usize(arg: Optional<UInt>) -> Optional<UInt> {
+    arg
+}
+func swift_reflect_option_isize(arg: Optional<Int>) -> Optional<Int> {
+    arg
+}
+func swift_reflect_option_f32(arg: Optional<Float>) -> Optional<Float> {
+    arg
+}
+func swift_reflect_option_f64(arg: Optional<Double>) -> Optional<Double> {
+    arg
+}
+func swift_reflect_option_bool(arg: Optional<Bool>) -> Optional<Bool> {
+    arg
 }
 
-func create_swift_option_u8_none() -> Optional<UInt8> {
-    nil
-}
