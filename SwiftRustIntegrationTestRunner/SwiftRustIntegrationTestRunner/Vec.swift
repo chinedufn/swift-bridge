df91f282e62b32755f1410c0e6f22b03f836f018
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2023-06-21 04:22:39 -0400

    Fix integration tests
    This commit fixes the `SwiftRustIntegrationTestRunner` project such that
    it now builds and runs the full test suite.
    
    Before this commit there was a recently introduced file that was not
    actually added to the project, so the project could not compile.
    
    CI was failing for the last few commits so this slipped through the
    cracks. This commit should fix CI.

--- SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunner/Vec.swift
+++ SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunner/Vec.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func swift_arg_vec_u8(vec _: RustVec<UInt8>) {
+func swift_arg_vec_u8(vec: RustVec<UInt8>) {
     assert(vec[0] == 1)
     assert(vec[1] == 2)
     assert(vec[2] == 3)
