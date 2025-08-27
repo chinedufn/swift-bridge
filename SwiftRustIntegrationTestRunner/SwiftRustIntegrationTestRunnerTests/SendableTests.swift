455e98639c714a576bf2c4c6467336a47bc06570
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2025-08-27 18:24:38 -0400

    Implement Sendable for `RustString` (#330)
    This commit adds an `@unchecked Sendable` protocol implementation for
    the `RustString` class, as well as documentation explaining that this is
    thread safe so long as the rules outlined in the book's `Safety` chapter
    are upheld.
    
    The following code now compiles:
    ```swift
    let rustString = RustString("hello world")
    
    Thread.detachNewThread {
        XCTAssertEqual(rustString.len(), 11)
    }
    ```

--- SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunnerTests/SendableTests.swift
+++ SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunnerTests/SendableTests.swift
@@ -8,11 +8,11 @@
 import XCTest
 @testable import SwiftRustIntegrationTestRunner
 
-/// Tests for the `#[swift_bridge(Sendable)]` attribute.
+/// Tests for Swift's `Sendable` protocol and the `#[swift_bridge(Sendable)]` attribute.
 ///
 /// For the corresponding Rust code, see `crates/swift-integration-tests/src/sendable_attribute.rs`.
 /// For the corresponding codegen tests, see `crates/swift-bridge-ir/src/codegen/codegen_tests/sendable_attribute.rs`.
-class SendableAttributeTests: XCTestCase {
+class SendableTests: XCTestCase {
     /// Verify that we can a Rust type that has the `#[swift_bridge(Sendable)]` attribute gets a `Sendable` protocol implementation.
     func testSendableExternRustType() throws {
         let sendableRustType = SendableRustType()
@@ -20,6 +20,15 @@ class SendableAttributeTests: XCTestCase {
         // Move the type to another thread.
         Thread.detachNewThread {
             let _ = sendableRustType;
+        }
+    }
+
+    /// Verify that we can send a `RustString` to another thread.
+    func testSendableRustString() {
+        let rustString = RustString("hello world")
+
+        Thread.detachNewThread {
+            XCTAssertEqual(rustString.len(), 11)
         }
     }
 }
